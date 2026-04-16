fcb84ed7e87e43e9b2f9e2eba2830e76e69438b4
Vitaly_M <vitalymatyha@gmail.com>
2026-04-16 13:22:43 +0300

    add serializers, ViewSets и Generic class

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+
+# Курс — через ViewSet (автоматически даёт все 5 операций)
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+# Уроки — через Generic-классы (каждый класс = одна операция)
+class LessonListView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonCreateView(generics.CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDetailView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDeleteView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
