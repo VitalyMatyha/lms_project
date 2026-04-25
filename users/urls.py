2c88c22e2280d2f55f32bf3f59e60af1858dc2d9
Vitaly_M <vitalymatyha@gmail.com>
2026-04-25 16:13:42 +0300

    add filters and sorted

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserUpdateView
+from .views import PaymentListView, UserUpdateView
 
 urlpatterns = [
+    path('payments/', PaymentListView.as_view(), name='payment-list'),
     path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
 ]
